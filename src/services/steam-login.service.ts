700032360b677b2a3cd69bced816b35941cd378c
tre1 <tobias.reski@iteratec.de>
2018-01-09 18:08:05 +0100

    activating mobile auth now works

--- src/services/steam-login.service.ts
+++ src/services/steam-login.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+
 declare var RSA: any;
 
 @Injectable()
@@ -26,10 +27,32 @@ export class SteamLoginService {
   }
 
   private encryptPWRSA(steamRSAData){
-
+    let timestamp = steamRSAData.timestamp;
     let publicKey = RSA.getPublicKey(steamRSAData.publickey_mod, steamRSAData.publickey_exp);
-    let encryptedData = RSA.encrypt("test", publicKey);
-    console.log(encryptedData);
+    let encryptedData = RSA.encrypt("Sy8kXy9aO9tb", publicKey);
+
+    this.logIntoSteam(encryptedData, timestamp)
+    console.log(publicKey);
+  }
+
+
+  private logIntoSteam(encryptedPW, timestamp){
+
+    let body = new FormData();
+    body.append("password", encryptedPW);
+    body.append("username", "liquidwater34");
+    body.append("donotcache", new Date().getTime().toString());
+    body.append("twofactorcode", "");
+    body.append("loginfriendlyname", "");
+    body.append("captchagid", "-1");
+    body.append("captcha_text", "");
+    body.append("emailsteamid", "");
+    body.append("rsatimestamp", timestamp);
+    body.append("remember_login", "false");
+    console.log(body);
+
+    this.http.post("https://steamcommunity.com/login/dologin/", body).subscribe(
+      steamResponse => console.log(steamResponse));
   }
 
 }
