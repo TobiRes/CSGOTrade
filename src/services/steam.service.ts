bfdedecc510fa0a148384da15b83a40f36dee404
Tobi <tobias.reski@gmail.com>
2018-01-27 13:45:19 +0100

    Now validating steam profile url

--- src/services/steam.service.ts
+++ src/services/steam.service.ts
@@ -12,6 +12,7 @@ export class SteamService {
 
   getCSGOInventory(steamInventoryURL: string) {
     return new Promise((resolve, reject) => {
+      steamInventoryURL = this.validateSteamURL(steamInventoryURL);
       this.http.get(steamInventoryURL + "/inventory/json/730/2").subscribe(
         (csgoInventoryData: any) => {
           if (!csgoInventoryData.success) {
@@ -24,6 +25,39 @@ export class SteamService {
         onerror => {
           reject(onerror);
         });
-    })
+    });
+  }
+
+  private validateSteamURL(steamInventoryURL: string) {
+    steamInventoryURL = this.checkForWholeURL(steamInventoryURL);
+    steamInventoryURL = this.checkForHTTP(steamInventoryURL);
+    return steamInventoryURL;
+  }
+
+  private checkForWholeURL(steamInventoryURL: string) {
+    if(steamInventoryURL.length <= 17){
+      if(steamInventoryURL.indexOf("/") > -1){
+        steamInventoryURL = steamInventoryURL.replace("/", "")
+      }
+      if(steamInventoryURL.match(/^[0-9]+$/) != null){
+        steamInventoryURL = "https://steamcommunity.com/profiles/" + steamInventoryURL;
+      } else {
+        steamInventoryURL =  "https://steamcommunity.com/id/" + steamInventoryURL;
+      }
+    }
+    if(steamInventoryURL.lastIndexOf("/") == steamInventoryURL.length -1){
+      steamInventoryURL = steamInventoryURL.substr(0, steamInventoryURL.length -1)
+    }
+    return steamInventoryURL;
+  }
+
+  private checkForHTTP(steamInventoryURL: string) {
+    if(steamInventoryURL.indexOf("http") != 0){
+      if(steamInventoryURL.indexOf("/") == 0){
+        steamInventoryURL = steamInventoryURL.substr(1, steamInventoryURL.length)
+      }
+      steamInventoryURL = "https://" + steamInventoryURL;
+    }
+    return steamInventoryURL;
   }
 }
