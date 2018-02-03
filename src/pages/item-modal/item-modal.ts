f2a9f527ef44ef21ea752d0368c337066669a60c
Tobi <tobias.reski@gmail.com>
2018-02-03 15:31:49 +0100

    Only show metjm if metjm is possible

--- src/pages/item-modal/item-modal.ts
+++ src/pages/item-modal/item-modal.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy} from '@angular/core';
 import {IonicPage, NavParams, ViewController} from 'ionic-angular';
-import {CSGOItem} from "../../models/csgoItem.model";
+import {CSGOItem, ItemType} from "../../models/csgoItem.model";
 import {InAppBrowser} from "@ionic-native/in-app-browser";
 import {takeUntil} from "rxjs/operators";
 import {Subject} from "rxjs/Subject";
@@ -47,6 +47,21 @@ export class ItemModalPage implements OnDestroy {
   ngOnDestroy() {
     this.destroyed$.next();
     this.destroyed$.complete();
+  }
+
+  metjmIsPossible() {
+    if(this.csgoItem.type != ItemType.key
+      && this.csgoItem.type != ItemType.collectible
+      && this.csgoItem.type != ItemType.gift
+      && this.csgoItem.type != ItemType.graffiti
+      && this.csgoItem.type != ItemType.tool
+      && this.csgoItem.type != ItemType.gloves
+      && this.csgoItem.type != ItemType.container
+      && this.csgoItem.type != ItemType.musicKit
+      && this.csgoItem.type != ItemType.tag){
+      return true;
+    }
+    return false;
   }
 
   private buildScreenshotScript() {
